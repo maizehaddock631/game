8a7c91caed791f5bb02d20cb4ab5301cfffa8d66
millaay <160738850+millaay@users.noreply.github.com>
2025-02-10 10:01:38 +0000

    Add files via upload

--- propertytycoon/dice.gd
+++ propertytycoon/dice.gd
@@ -1,9 +1,12 @@
+extends Node2D
 class_name Dice
 
-extends Node2D
-
+@onready var dice_animation = $DiceAnimation
 var random = RandomNumberGenerator.new()
+
 
 func roll() -> int:
 	var number = random.randi_range(1,6)
+	print("you rolled a " + str(number));
+	dice_animation.play("rolling")
 	return number

49a450aa899c4347d27e49272fdb83eea50173b4
maizehaddock631 <hayejhardelle34@gmail.com>
2025-02-10 12:27:32 +0000

    Add files via upload

--- propertytycoon/banker.gd
+++ propertytycoon/banker.gd
@@ -8,3 +8,13 @@ var properties = []
 func pay(amount : int, who : Player) -> void:
 	who.balance += amount
 	balance -= amount
+
+func sell_property(player: Player, tile: PropertyTile) -> void:
+	if tile.propertycost > player.balance:
+		print("Sorry. Not enough funds")
+	else:
+		tile.propertyowner = player
+		balance += tile.propertycost
+		player.balance -= tile.propertycost
+		player.properties.append(tile)
+		properties.erase(tile)

35cb77180cd68f29b1982c5a05e542a751468f6c
Jhardelle <hayejhardelle34@gmail.com>
2025-02-23 13:54:53 +0000

    23rd feb update

--- propertytycoon/player.gd
+++ propertytycoon/player.gd
@@ -25,11 +25,11 @@ func roll() -> int:
 
 func buy_property(property: PropertyTile, bank: Banker) -> void:
 	if self.balance < property.propertycost:
-		print("Sorry! You don't have enough funds!")
+		print("Sorry! You don't have enough funds to purchase ", property.propertyname)
 	if not self.has_completed_loop:
 		print("You have to have completed a loop first!")
 	if property.propertyowner is Player:
-		print("Sorry! Someone already owns this property")
+		print("Sorry! Someone already owns ", property.propertyname)
 	
 	if self.balance >= property.propertycost and self.has_completed_loop and property.propertyowner is not Player:
 		self.properties.append(property)
@@ -37,11 +37,11 @@ func buy_property(property: PropertyTile, bank: Banker) -> void:
 		bank.properties.erase(property)
 		self.balance -= property.propertycost
 		bank.balance += property.propertycost
-		print("You have bought", property.propertyname)
+		print("You have bought ", property.propertyname)
 
 func sell_property(property: PropertyTile, bank: Banker) -> void:
 	if property not in self.properties:
-		print("You don't even own this property!")
+		print("You don't even own ", property.propertyname)
 	else:
 		var new_sell_price = property.propertycost / 2
 		self.balance += new_sell_price
@@ -49,4 +49,28 @@ func sell_property(property: PropertyTile, bank: Banker) -> void:
 		bank.properties.append(property)
 		self.properties.erase(property)
 		property.propertyowner = bank
-		print("You have sold", property.propertyname)
+		print("You have sold ", property.propertyname)
+
+func mortgage_property(property: PropertyTile, bank: Banker) -> void:
+	pass
+	
+func unmortgage_property(property: PropertyTile, bank: Banker ) -> void:
+	pass
+
+func use_getoutofjailfree() -> void:
+	if self.injail == false:
+		print("You're not even in jail!")
+	if self.get_out_of_jail_free == 0:
+		print("Sorry! You don't have any get out of jail free cards.")
+	else:
+		self.get_out_of_jail_free -= 1
+		self.injail = false
+		print("You have used one of your get out of jail free cards. You're no longer in jail")
+
+func declare_bankruptcy(bank: Banker) -> void:
+	bank.balance += self.balance
+	self.balance = 0
+	print("You have now been declared bankrupt and you're eliminated from the game")
+	
+	
+	
--- propertytycoon/tests/test_player.gd
+++ propertytycoon/tests/test_player.gd
@@ -1,16 +1,20 @@
-extends Node
+extends GutTest
 
-func _ready():
-	passtest_buyproperty()
-	failtest_buyproperty()
+var player
+var property
+var bank
 
-func passtest_buyproperty():
-	var property = PropertyTile.new()
-	var player = Player.new()
-	var bank = Banker.new()
+func _before_each():
+	print("Setting up before all tests")
+	player = Player.new()
+	property = PropertyTile.new()
+	bank = Banker.new()
+	
+func test_buypropertypass():
 	
 	property.propertycost = 500
 	player.has_completed_loop = true
+	property.propertyname = "The Old Creek"
 	
 	player.buy_property(property, bank)
 	
@@ -20,12 +24,10 @@ func passtest_buyproperty():
 	assert(property in player.properties, "Doesn't own this property")
 	assert(property not in bank.properties)
 	
-func failtest_buyproperty():
-	var property = PropertyTile.new()
-	var player = Player.new()
-	var bank = Banker.new()
+func test_buypropertyfail():
 	
 	property.propertycost = 2000
+	property.propertyname = "The Old Creek"
 	player.has_completed_loop = false
 	bank.properties.append(property)
 	
@@ -37,11 +39,9 @@ func failtest_buyproperty():
 	assert(property in bank.properties, "Bank should own this property")
 
 func test_sellproperty():
-	var property = PropertyTile.new()
-	var player = Player.new()
-	var bank = Banker.new()
 	
 	property.propertycost = 2000
+	property.propertyname = "The Old Creek"
 	player.properties.append(property)
 	property.propertyowner = player
 	
@@ -53,5 +53,28 @@ func test_sellproperty():
 	assert(bank.balance == 49000, "Bank should've spent money")
 	assert(property.propertyowner == bank, "Property owner should be bank")
 	
+func test_getoutofjailfreepass():
+	player.injail = true
+	player.get_out_of_jail_free = 2
 	
+	player.use_getoutofjailfree()
+	
+	assert(player.get_out_of_jail_free == 1, "Player should have one less jail free cards")
+	assert(player.injail == false, "Player should no longer be in jail")
+
+func test_getoutofjailfreefail():
+	player.injail = true
+	player.get_out_of_jail_free = 0
+	
+	player.use_getoutofjailfree()
+	
+	assert(player.get_out_of_jail_free == 0, "Number of jail free cards should be the same")
+	assert(player.injail == true, "Player should still be in jail")
+
+func test_declarebankruptcy():
+	player.declare_bankruptcy()
+	
+	assert(player.is_bankrupt == true, "Player should be bankrupt")
+	assert(player.balance == 0, "Player should have no money")
+	assert(bank == 51000, "Bank should have players money")
 	

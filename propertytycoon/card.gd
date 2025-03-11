23158df5123ce7d239d26e4e6c3a58731612cb4a
maizehaddock631 <hayejhardelle34@gmail.com>
2025-03-11 13:02:13 +0000

    Delete propertytycoon directory

--- propertytycoon/card.gd
+++
@@ -1,1 +0,0 @@
-class_name Card extends Node2D
--- propertytycoon/property_tile.gd
+++
@@ -1,7 +0,0 @@
-class_name PropertyTile extends Tile
-
-@export var propertyname: String
-@export var propertycost : int
-@export var propertyowner : Node
-@export var rent : int
-@export var numofhouses: int
